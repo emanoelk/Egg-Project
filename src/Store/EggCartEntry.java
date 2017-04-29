dae2180ea28eda8be7109aa87cf57834a1b564d7
emanoelk <khemanoel@gmail.com>
2017-04-29 12:46:53 -0700

    full

--- src/Store/EggCartEntry.java
+++ src/Store/EggCartEntry.java
@@ -1,4 +1,4 @@
-package Store;
+package store;
 public class EggCartEntry {
 		String name;
 		double price;
--- src/store/Seller.java
+++ src/store/Seller.java
@@ -1,4 +1,4 @@
-package Store;
+package store;
  
 import java.io.IOException;
 import java.sql.Connection;
